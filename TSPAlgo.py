aadcf4da81d31135fb87e3d5273453de691090f6
James-K-WNEU <jk609989@wne.edu>
2025-11-05 12:01:29 -0500

    uploaded 11/5/2025
    last  updated 12:01 PM, Nov 5, 2025

--- TSPAlgo.py
+++ TSPAlgo.py
@@ -6,16 +6,27 @@ import itertools
 import time
 
 def test():
+    # Generate a randomized graph with vertex names as follows:
     R = RandomGraph(["A", "B", "C", "D", "E", "F", "G", "H"])
+    # Print the cost/distance matrix for the randomized graph. We can use this to
+    # represent our "distance function"
+    print("Cost matrix for randomized graph R:")
+    for r in R.CostMatrix(True):
+        print(str(r))
+
     NNP = NearestNeighbor(R, "A", True)
-    print("Nearest Neighbor Path Result:", NNP)
-    print("Total Path Weight:", R.PathWeight(NNP, True), "\n")
-    #TSP.TraceGraphPath(R, NNP, False, True, True)
+    print("\n Nearest Neighbor Path Result:", NNP)
+    print("Total Path Weight:", R.PathWeight(NNP, True))
 
     BFP = BruteForce(R, "A", True)
-    print("Brute Force Path Result:", BFP)
-    print("Total Path Weight:", R.PathWeight(BFP, True), "\n")
-    TSP.TracePath(R, BFP, "Brute Force Path", False, True, True, "blue")
+    print("\n Brute Force Path Result:", BFP)
+    print("Total Path Weight:", R.PathWeight(BFP, True))
+
+    NIP = NearestInsert(R, "A", True)
+
+    PathDict = {"Nearest Neighbor": NNP, "Brute Force": BFP}
+
+    #TSP.TracePaths(R, PathDict, False, True, True)
 
 def RandomGraph(NameSet):
     # Generate a random graph with vertex names from the set "NameSet".
@@ -29,8 +40,8 @@ def RandomGraph(NameSet):
 def BruteForce(GraphObj, StartVertName, CycleBool):
     # Check every unique path originating from the starting vertex, then
     # compare them to find the shortest one. Guarenteed to find an exact answer,
-    # but computationally inefficient (O(n!) time completixty).
-    # If CycleBool == True, include the edge that gets us back to our starting vertex.
+    # but computationally inefficient (O(n!) time completixty). If CycleBool == True, 
+    # include the edge that gets us back to our starting vertex.
     finalpath = None
     pathdict = {}
     pathlist =[]
@@ -106,11 +117,48 @@ def NearestNeighbor(GraphObj, StartVertName, CycleBool):
                         if X in finalpath:
                             GraphVertexNames.remove(X)
                             
-
-
         if CycleBool == True:
             finalpath.append(StartVertName)
 
+    return finalpath
+
+def NearestInsert(GraphObj, StartVertName, CycleBool):
+    # Start with the starting vertex. At each step of the process, add
+    # to our "final path" the vertex with the lowest distance to any vertex
+    # already in our "final path". Repeat this until every vertex in the graph
+    # has been visited once. If CycleBool == True, add the starting vertex to the
+    # end of our path so it becomes a cycle.
+    finalpath = []
+    
+    GraphVertices = GraphObj.GetVertexNames()
+    UnvisitedVertices = GraphObj.GetVertexNames()
+
+    if GraphObj.NameInGraph(StartVertName) == True:
+        finalpath.append(StartVertName)
+        UnvisitedVertices.remove(StartVertName)
+
+        while len(UnvisitedVertices) > 0:
+            vertexdistances = []
+            closestvertdict = {}
+            for v in finalpath:
+                distdict = {}
+
+                for w in GraphVertices:
+                    if w not in finalpath:
+                        distdict[w] = GraphObj.GetDistance(v, w, False)
+                    
+                vertexdistances.append(distdict)
+                SortedDistances = dict(sorted(distdict.items(), key=lambda item: item[1]))
+
+                closestvert = min(SortedDistances, key=SortedDistances.get)
+                closestvertdict[closestvert] = distdict[closestvert]
+
+            mindist = min(closestvertdict, key=SortedDistances.get)
+            print(vertexdistances, mindist)
+            UnvisitedVertices.pop(0)
+
+
+    
     return finalpath
 
 test()
